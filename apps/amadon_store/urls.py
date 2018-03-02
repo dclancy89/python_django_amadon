1c3d387e3a13c13f348fc66c0f977bef2dd813bc
Daniel <clancy.daniel@gmail.com>
2018-03-02 11:13:10 -0600

    finished store

--- apps/amadon_store/urls.py
+++ apps/amadon_store/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index)
+    url(r'^$', views.index),
+    url(r'^amadon/process$', views.process),
+    url(r'^amadon/checkout$', views.checkout),
+    url(r'^amadon/goback$', views.goback)
 ]
--- apps/amadon_store/views.py
+++ apps/amadon_store/views.py
@@ -2,5 +2,42 @@ from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
 def index(request):
-	response = "Success!"
-	return HttpResponse(response)
+	
+
+	return render(request, 'amadon_store/index.html')
+
+def process(request):
+
+	if request.session.get('total_items') == None:
+		request.session['total_items'] = 0
+
+	if request.session.get('total_cost') == None:
+		request.session['total_cost'] = 0
+	
+	price = {
+		'01': '19.99',
+		'02': '29.99',
+		'03': '4.99',
+		'04': '49.99'
+	}
+
+	request.session['produt_id'] = request.POST['product']
+	request.session['product_price'] = price[request.POST['product']]
+	request.session['product_quantity'] = request.POST['quantity']
+	request.session['order_total'] = float(price[request.POST['product']]) * int(request.POST['quantity'])
+	
+	request.session['total_items'] += int(request.POST['quantity'])
+	request.session['total_cost'] += float(price[request.POST['product']]) * int(request.POST['quantity'])
+
+	return redirect('/amadon/checkout')
+
+def checkout(request):
+	return render(request, 'amadon_store/checkout.html')
+
+def goback(request):
+	request.session.pop('produt_id')
+	request.session.pop('product_price')
+	request.session.pop('product_quantity')
+	request.session.pop('order_total')
+
+	return redirect('/')
